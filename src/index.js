007e153ce4118fddcfa61a588bda65823fe78ab3
Giang98 <nguyenhoanggiang290998@gmail.com>
2022-11-28 16:50:58 +0700

    fix 28.11.22.16.50

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_LOGIN_GOOGLE_CLIENT_ID}> 
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
